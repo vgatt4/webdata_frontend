a2906a010e7f1a5a9261c9dfe9932655db8b7650
vgatt4 <119984745+vgatt4@users.noreply.github.com>
2022-12-07 14:20:47 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <HeaderTop bookName="Web Series" authorName="United States of America" />
+
+    <BookBox :books="books" />
+
+</div>
+</template>
+
+<script>
+
+import HeaderTop from './components/HeaderTop.vue'
+import BookBox from './components/BookBox.vue'
+
+export default {
+  name: 'App',
+  components: {    
+    HeaderTop,
+    BookBox
+  },
+
+  data(){
+    return {
+      books: []
+    }
+
+  },
+
+  methods: {
+
+    // promises
+
+    async fetchBooks(){
+      const res = await fetch('https://varshafinalproject.herokuapp.com/api')
+      const data = await res.json()
+      console.log( data )
+      return data
+    }
+
+
+  },
+  async created(){
+    this.books = await this.fetchBooks()
+  }
+}
+
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 0.5em;
+}
+
+</style>
